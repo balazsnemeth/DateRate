1d56e76bc1f5afeca6cf4b6634eada58b7decfd2
Balazs Nemeth <balazs.nemeth@lhsystems.com>
2013-10-13 22:18:34 +0200

    popups added

--- daterate/js/daterate.js
+++ daterate/js/daterate.js
@@ -1,5 +1,27 @@
 $(function() {
-        
-        //Insert code here
-        
-        });
+	
+/*        $("#createDateRateButton").animate({
+    		width:"100%",
+  		} , 2000, "swing" , function() {
+     	  console.log( "Animation complete.");
+    	});*/
+
+	$('.startAnimation').each(function(index){
+			console.log(index);
+		setTimeout($.proxy(function() {
+			console.log('s'+index);
+			$( this ).addClass('transformWidth');
+		},this), index*300);
+	});
+/*
+	$("#RatingStatus").click( function()
+           {
+				showConfirmation('Please, enter the shared evaluation code:','Evaluation Code','Show rating','Cancel',function(button){
+				    console.log('You selected button ' + button);
+				    //$.mobile.changePage("page6.html");
+				});
+           }
+      );
+	*/
+
+});
